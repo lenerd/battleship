00064cfa3f5c6fb1342dc9603d2e6199f824a2da
Lennart Braun <lenerd@posteo.de>
2017-02-10 22:33:36 +0100

    fixed util test naming

--- src/test_util.cpp
+++ src/test_util.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "util.hpp"
 
-TEST(RandomBytesTest, UtilTest)
+TEST(UtilTest, RandomBytesTest)
 {
     bytes_t val(random_bytes(42));
     ASSERT_EQ(val.size(), 42);
