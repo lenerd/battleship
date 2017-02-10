b93a184cebcded38f804770e7219ea2280f26e4a
Lennart Braun <lenerd@posteo.de>
2017-02-10 23:00:59 +0100

    misc tests

---
+++ src/test_misc.cpp
@@ -0,0 +1,52 @@
+#include <sstream>
+#include <gtest/gtest.h>
+#include "options.hpp"
+#include "util.hpp"
+
+TEST(MiscTest, RandomBytes)
+{
+    bytes_t val(random_bytes(42));
+    ASSERT_EQ(val.size(), 42);
+}
+
+TEST(MiscTest, RoleEnum)
+{
+    std::stringstream ss;
+    Role role;
+    ss << Role::client;
+    ASSERT_EQ(ss.str(), "client");
+    ss >> role;
+    ASSERT_EQ(role, Role::client);
+
+    ss = std::stringstream();
+    ss << Role::server;
+    ASSERT_EQ(ss.str(), "server");
+    ss >> role;
+    ASSERT_EQ(role, Role::server);
+
+    ss = std::stringstream();
+    ss << "foobar";
+    ASSERT_EQ(ss.str(), "foobar");
+    ASSERT_ANY_THROW(ss >> role);
+}
+
+TEST(MiscTest, UITypeEnum)
+{
+    std::stringstream ss;
+    UIType role;
+    ss << UIType::ncurses;
+    ASSERT_EQ(ss.str(), "ncurses");
+    ss >> role;
+    ASSERT_EQ(role, UIType::ncurses);
+
+    ss = std::stringstream();
+    ss << UIType::web;
+    ASSERT_EQ(ss.str(), "web");
+    ss >> role;
+    ASSERT_EQ(role, UIType::web);
+
+    ss = std::stringstream();
+    ss << "foobar";
+    ASSERT_EQ(ss.str(), "foobar");
+    ASSERT_ANY_THROW(ss >> role);
+}
--- src/test_util.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include "util.hpp"
-
-TEST(UtilTest, RandomBytesTest)
-{
-    bytes_t val(random_bytes(42));
-    ASSERT_EQ(val.size(), 42);
-}
