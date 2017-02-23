7b1108a7b92925e9b8e9f810f16b66d459dfdbf8
Lennart Braun <lenerd@posteo.de>
2017-02-23 16:15:15 +0100

    added bitset conversion functions

--- src/misc/util.hpp
+++ src/misc/util.hpp
@@ -1,6 +1,7 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
+#include <bitset>
 #include <cstddef>
 #include <cstdint>
 #include <vector>
@@ -9,5 +10,29 @@ using bytes_t = std::vector<uint8_t>;
 using coords_t = std::pair<size_t, size_t>;
 
 bytes_t random_bytes(size_t n);
+
+template <size_t len>
+std::bitset<len> string_to_bits(std::string s)
+{
+    return std::bitset<len>(std::string(s.crbegin(), s.crend()));
+}
+
+template <size_t len>
+bytes_t bits_to_bytes(std::bitset<len> bits)
+{
+    bytes_t bytes;
+    size_t num_bytes{bits.size() / 8};
+    for (size_t n{0}; n < num_bytes; ++n)
+    {
+        uint8_t byte{0};
+        for (size_t i{0}; i < 8; ++i)
+        {
+            byte |= bits[8 * n + i] << i;
+        }
+
+        bytes.push_back(byte);
+    }
+    return bytes;
+}
 
 #endif // UTIL_HPP
--- src/test/test_misc.cpp
+++ src/test/test_misc.cpp
@@ -9,6 +9,27 @@ TEST(MiscTest, RandomBytes)
     ASSERT_EQ(val.size(), 42);
 }
 
+TEST(MiscTest, StringToBits)
+{
+    std::string bitstr("10001011""11101111");
+    auto bits{string_to_bits<16>(bitstr)};
+    for (size_t i{0}; i < bitstr.size(); ++i)
+    {
+        ASSERT_EQ(bits[i], bitstr[i] - '0');
+    }
+}
+
+TEST(MiscTest, BitsToBytes)
+{
+    auto bits{string_to_bits<32>("01111011"
+                                 "10110101"
+                                 "01111101"
+                                 "11110111")};
+    bytes_t bytes{bits_to_bytes(bits)};
+    bytes_t correct_bytes{0xde, 0xad, 0xbe, 0xef};
+    ASSERT_EQ(bytes, correct_bytes);
+}
+
 TEST(MiscTest, RoleEnum)
 {
     std::stringstream ss;
