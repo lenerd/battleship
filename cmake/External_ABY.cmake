7f90d837584d626e258c22eae0551a6bacec767e
Lennart Braun <lenerd@posteo.de>
2017-02-23 19:28:04 +0100

    updated cmake files
    * forward compiler
    * copy ot sources into aby tree

--- cmake/External_ABY.cmake
+++ cmake/External_ABY.cmake
@@ -5,17 +5,20 @@ configure_file(
 )
 
 ExternalProject_Add(ABY
-    DEPENDS MIRACL OTExtension
+    DEPENDS MIRACL
     SOURCE_DIR "${PROJECT_SOURCE_DIR}/extern/ABY"
     INSTALL_DIR "${Battleship_INSTALL_PREFIX}"
     PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
         "${CMAKE_CURRENT_BINARY_DIR}/ABY.CMakeLists.txt"
         "<SOURCE_DIR>/CMakeLists.txt"
     COMMAND ${CMAKE_COMMAND} -E copy_directory
-        "${Battleship_INSTALL_PREFIX}/include/ot"
+        "${PROJECT_SOURCE_DIR}/extern/OTExtension/ot"
         "<SOURCE_DIR>/src/abycore/ot"
     CMAKE_ARGS
         -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
         -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
         -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+        -DCMAKE_CXX_COMPILER:PATH=${CMAKE_CXX_COMPILER}
+        -DCMAKE_C_COMPILER:PATH=${CMAKE_C_COMPILER}
+		-DBUILD_EXE=ON
 )
