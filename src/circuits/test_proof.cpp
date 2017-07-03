baeb193275054f9b10bf39f4502690182acc16d0
Lennart Braun <lenerd@posteo.de>
2017-07-04 01:36:42 +0200

    add program options to proof test

--- src/circuits/test_proof.cpp
+++ src/circuits/test_proof.cpp
@@ -1,26 +1,54 @@
 #include <iostream>
 #include <sstream>
 #include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 #include "proof.hpp"
 #include "misc/options.hpp"
 #include "network/tcp_connection.hpp"
 
 
+namespace po = boost::program_options;
+
+
+Options parse_arguments(int argc, char* argv[])
+{
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "produce help message")
+        ("role,r", po::value<Role>()->required(), "role")
+        ("address", po::value<std::string>()->default_value("127.0.0.1"), "address")
+        ("game-port", po::value<uint16_t>()->default_value(6666), "game port")
+        ("aby-port", po::value<uint16_t>()->default_value(6677), "aby port")
+    ;
+    po::variables_map vm;
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    }
+    catch (po::error &e)
+    {
+        std::cerr << "Error parsing arguments: " << e.what() << "\n";
+        std::cerr << "\n" << desc << "\n";
+        exit(EXIT_FAILURE);
+    }
+
+    if (vm.count("help"))
+    {
+        std::cerr << desc << "\n";
+        exit(EXIT_FAILURE);
+    }
+    Options options;
+    options.role = vm["role"].as<Role>();
+    options.address = vm["address"].as<std::string>();
+    options.game_port = vm["game-port"].as<uint16_t>();
+    options.aby_port = vm["aby-port"].as<uint16_t>();
+    return options;
+}
+
 int main(int argc, char* argv[])
 {
-    Options options;
-    if (argc < 2)
-    {
-        std::cerr << "usage: ./test_bd.exe <role>\n";
-        return 1;
-    }
-    std::stringstream ss(argv[1]);
-    ss >> options.role;
-    std::cout << "Role: " << options.role << std::endl;
-
-    options.address = "127.0.0.1";
-    options.game_port = 6666;
-    options.aby_port = 6677;
+    auto options{parse_arguments(argc, argv)};
 
     boost::asio::io_service io_service;
     auto conn{TCPConnection::from_role(options.role, io_service, options.address, options.game_port)};
