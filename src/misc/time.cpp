0bdf76a663b51940a9d7d472d1ae93607226f0d3
Lennart Braun <lenerd@posteo.de>
2017-07-04 17:23:01 +0200

    millisecond unix time

--- src/misc/time.cpp
+++ src/misc/time.cpp
@@ -7,15 +7,29 @@
 
 std::string print_now(bool ms, bool human)
 {
+    auto tp = std::chrono::system_clock::now();
     if (human)
     {
-        auto tp = std::chrono::system_clock::now();
         return print_timestamp_human(tp, ms);
     }
     else
     {
-        auto unix_timestamp = std::chrono::seconds(std::time(NULL));
-        return std::to_string(unix_timestamp.count());
+        auto unix_time = tp.time_since_epoch();
+        auto seconds = std::chrono::duration_cast<std::chrono::seconds>(unix_time);
+        if (ms)
+        {
+            auto fraction = tp - seconds;
+            auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(fraction.time_since_epoch());
+            std::stringstream ss;
+            ss << seconds.count()
+               << "." << std::setw(3) << std::setfill('0')
+               << milliseconds.count();
+            return ss.str();
+        }
+        else
+        {
+            return std::to_string(seconds.count());
+        }
     }
 }
 
