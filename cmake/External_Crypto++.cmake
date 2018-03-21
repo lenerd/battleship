82cb066787a68f36c7845b527a1bde30de7ee1b9
Lennart Braun <lenerd@posteo.de>
2018-03-21 02:27:53 +0100

    update submodule weidai11/cryptopp to v6.1.0
    Crypto++'s CMake files were moved to noloader/cryptopp-cmake, which is
    now also included as submodule.

--- cmake/External_Crypto++.cmake
+++ cmake/External_Crypto++.cmake
@@ -1,6 +1,10 @@
 ExternalProject_Add(Crypto++
     SOURCE_DIR "${PROJECT_SOURCE_DIR}/extern/cryptopp"
     INSTALL_DIR "${Battleship_INSTALL_PREFIX}"
+    PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        "${PROJECT_SOURCE_DIR}/extern/cryptopp-cmake/CMakeLists.txt"
+        "${PROJECT_SOURCE_DIR}/extern/cryptopp-cmake/cryptopp-config.cmake"
+        "<SOURCE_DIR>/"
     CMAKE_ARGS
         -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
         -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
